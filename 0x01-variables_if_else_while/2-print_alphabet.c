5693c40fdbc0e5573acb0db4a535baf505d91a51
Nihal El oudiki <eloudikinihal@gmail.com>
2023-09-08 16:32:51 -0700

    My new commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: A C program that print with put function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char x = "a";
+
+	while (x <= "z")
+	{
+		putchar(x);
+		x++;
+	}
+	putchar("\n");
+	return (0);
+}
