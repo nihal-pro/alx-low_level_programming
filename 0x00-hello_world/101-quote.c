9a753d1c9822d10a6f0cbeb730f0acb349d27f99
Nihal El oudiki <eloudikinihal@gmail.com>
2023-09-07 19:04:23 -0700

    My new commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - A program that print a line through putchar  function
  * Return: 1 (Success)
  */
 int main(void)
 {
-putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
