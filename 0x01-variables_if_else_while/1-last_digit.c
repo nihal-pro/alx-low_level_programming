b9ade63c8a82c71bb74b05e1096bbdb92b529219
Nihal El oudiki <eloudikinihal@gmail.com>
2023-09-08 15:27:44 -0700

    My new commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 		printf("Last digit of %d is greater than 5\n", n);
 	if (n == 0)
 		printf("Last digit of %d is zero\n", n);
-	if (n < 6&&n != 0)
+	if (n < 6 && n != 0)
 		printf("Last digit of %d is less than 6 and not 0\n", n);
 	return (0);
 }
