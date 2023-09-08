e5ab89198a008cafa00a84ae6c5e7b64033a1aee
Nihal El oudiki <eloudikinihal@gmail.com>
2023-09-07 17:18:01 -0700

    My new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,10 @@
-#!/bin/bash
-puts("\*"Programming is like building a multilingual puzzle")
-return 0
+#include <stdio.h>
+/**
+ * main - A program that print a line through puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
