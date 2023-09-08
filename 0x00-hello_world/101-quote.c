6e2d593a5bc5b10f31d84d8c86361f82a9856255
Nihal El oudiki <eloudikinihal@gmail.com>
2023-09-07 19:21:17 -0700

    My new commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that print a line through fput function
+ * main - A program that print a line through write function
  * Return: 1 (Success)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
