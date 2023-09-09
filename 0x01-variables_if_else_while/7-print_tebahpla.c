29f1663c28a74a8e3a1ed9b952ed11a8357ab704
Nihal El oudiki <eloudikinihal@gmail.com>
2023-09-08 17:35:20 -0700

    My new commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: A C program that print with put function
+ * Return: always 0
+ */
+int main(void)
+{
+	char i = 'z';
+
+	while (i >= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
