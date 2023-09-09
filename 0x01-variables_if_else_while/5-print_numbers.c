f91698404262142f248600a34004ca404069f0cb
Nihal El oudiki <eloudikinihal@gmail.com>
2023-09-08 17:21:06 -0700

    My new commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: A C program that print with put function
+ * Return: always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
