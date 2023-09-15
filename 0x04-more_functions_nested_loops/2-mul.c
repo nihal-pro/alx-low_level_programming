f9332a93d195fefb6b7420d9aaea0c1dea257187
Nihal El oudiki <eloudikinihal@gmail.com>
2023-09-15 03:27:43 -0700

    My new commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - fonction
+ * mul: fonction that multiplies two integers
+ * @a: parametre
+ * @b: parametre
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
