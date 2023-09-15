5b5ec6bb7abaebbe300c81eedb7ec58b7a732fa2
Nihal El oudiki <eloudikinihal@gmail.com>
2023-09-15 14:48:54 -0700

    My new commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - fonction
+ * print_diagonal: fonction that print diagonal
+ * @n: parametre
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar(32);
+			}
+
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
