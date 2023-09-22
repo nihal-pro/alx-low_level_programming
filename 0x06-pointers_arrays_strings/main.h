b28e0fc42021aa83350332fe8cfe31b93412a079
Nihal El oudiki <eloudikinihal@gmail.com>
2023-09-21 17:59:25 -0700

    My new commit

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+
+
+#endif /* MAIN_H */
