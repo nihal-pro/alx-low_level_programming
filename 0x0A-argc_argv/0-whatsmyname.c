bd7d7330eb05bf64eea3c3b1f0e9af4e49025bb7
Nihal El oudiki <eloudikinihal@gmail.com>
2023-10-02 15:50:03 -0700

    My new commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print program name
+ * @argc: parametre
+ * @argv: parametre
+ * Return: 0
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	/*(void)argc;*/
+	printf("%s\n", argv[0]);
+	return (0);
+}
