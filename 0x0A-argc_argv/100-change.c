41653521cff3326983b814ccb7a97d7134c42410
Nihal El oudiki <eloudikinihal@gmail.com>
2023-10-02 16:39:44 -0700

    My new commit

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: 0 for success.
+ */
+int main(int argc, char *argv[])
+{
+	int change = 0;
+	int num;
+
+	if (argc == 2)
+	{
+	num = atoi(argv[1]);
+	if (num > 0)
+	{
+	while (num >= 25)
+	{
+	num -= 25;
+	change++;
+	}
+	while (num >= 10)
+	{
+	num -= 10;
+	change++;
+	}
+	while (num >= 5)
+	{
+	num -= 5;
+	change++;
+	}
+	while (num >= 2)
+	{
+	num -= 2;
+	change++;
+	}
+	while (num >= 1)
+	{
+	num -= 1;
+	change++;
+	}
+	}
+
+	printf("%d\n", change);
+	return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
