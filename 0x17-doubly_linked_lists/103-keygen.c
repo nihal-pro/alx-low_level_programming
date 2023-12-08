5979b292073392a45a13fc96cac90875c0f3c93a
NIHAL <nihaleloudiki16@gmail.com>
2023-12-07 17:34:36 -0800

    handle task

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *@argc: argument count
+ *@av: pointer to arguments
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(int argc, __attribute__((__unused__)) char **av)
+{
+	int *p = NULL;
+
+	if (argc != 2)
+		*p = 5;
+
+	return (0);
+}
