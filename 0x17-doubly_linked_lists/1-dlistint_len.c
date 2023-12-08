7addda5e37c55ce3ff2d52a56902ad1e21b809a8
NIHAL <nihaleloudiki16@gmail.com>
2023-12-07 16:59:55 -0800

    handle task

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - a function that returns the number of elements
+ *                    in a linked dlistint_t list
+ * @h: a pointer to linked list.
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+		h = h->next, count++;
+	return (count);
+}
